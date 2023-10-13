bdb50b687e287da343156ce5b5cb5586fcc17894
webflyx <zavala180395@gmail.com>
2023-10-13 13:53:51 +0300

    Added install translations

--- assets/app.js
+++ assets/app.js
@@ -8,11 +8,50 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+// Hamburget Menu
 const menu = document.getElementById('main-menu')
 document.getElementById('hamburger').addEventListener('click', () => {
-    if(menu.classList.contains('hidden')){
+    if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
         menu.classList.add('hidden');
     }
 })
+
+// Show / Hide Alerts
+function showAlert($id, $time) {
+    setTimeout(() => {
+        document.getElementById($id).classList.add('invisible', 'opacity-0');
+    }, $time);
+}
+
+// Notification
+document.getElementById('notification-close').addEventListener('click', () => {
+    showAlert('notification', 0);
+})
+
+if (!document.getElementById('notification').classList.contains('invisible')) {
+    showAlert('notification', 5000);
+}
+
+//Profile
+const profileTabs = document.querySelectorAll('.profile-tab');
+const profileContent = document.querySelectorAll('.profile-content');
+profileTabs.forEach(el => {
+    el.addEventListener('click', () => {
+        var thisTab = el.getAttribute('id');
+        var thisContent = thisTab.replace('tab-', '');
+        console.log(thisContent);
+
+        profileTabs.forEach(elTab => {
+            elTab.classList.remove('border-b-2', 'border-sky-500', 'text-sky-500');
+        })
+        profileContent.forEach(elContent => {
+            elContent.classList.add('hidden');
+        })
+
+        el.classList.add('border-b-2', 'border-sky-500', 'text-sky-500');
+        document.getElementById(thisContent).classList.remove('hidden');
+
+    })
+});
---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('dashboard/dashboard.html.twig', []);
+    }
+
+    #[Route('/dashboard/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('dashboard/profile.html.twig', []);
+    }
+}
